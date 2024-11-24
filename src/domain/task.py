babf688d0c8a5b58089dedc11b962eb52b2df5b2
Kirill <kdberdyshev@gmail.com>
2024-11-25 00:04:08 +0300

    Add basic structure and task model

---
+++ src/domain/task.py
@@ -0,0 +1,15 @@
+from sqlmodel import Field, SQLModel, Column
+from typing import Optional
+from datetime import datetime
+
+class Task(SQLModel):
+    __tablename__ = "task"
+    id: Optional[int] = Field(nullable=False, primary_key=True)
+    user_id: str = Field(nullable=False)
+    task_name: str = Field(nullable=False)
+    tags: Optional[set[str]] = Field(nullable=True)
+    comments: Optional[str] = Field(nullable=True)
+    created_at: datetime = Field(default=datetime.now())
+    state: str = Field(nullable=False)
+    deadline: Optional[datetime] = Field(nullable=True)
+    
