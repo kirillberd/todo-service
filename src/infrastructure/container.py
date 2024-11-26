56e0ea18bd9e41379b3bcdfd8e4d9f1b953856b1
Kirill <kdberdyshev@gmail.com>
2024-11-26 13:46:16 +0300

    Add dependency injector container

---
+++ src/infrastructure/container.py
@@ -0,0 +1,25 @@
+from dependency_injector import containers, providers
+from sqlalchemy import Engine
+from sqlmodel import create_engine
+from applicaiton.services.task_service import TaskService
+from infrastructure.repositories.task_repository import TaskRepository
+
+
+class Container(containers.DeclarativeContainer):
+    config = providers.Configuration()
+
+    sql_engine: Engine = providers.Singleton(
+        create_engine,
+        url=providers.Callable(
+                       lambda user, password, host, port, dbname: f"postgresql://{user}:{password}@{host}:{port}/{dbname}",
+            config.postgres_user,
+            config.postgres_password,
+            config.postgres_host,
+            config.postgres_port,
+            config.postgres_dbname,
+        )
+    )
+
+    task_repository: TaskRepository = providers.Singleton(
+        TaskRepository, engine=sql_engine
+    )
